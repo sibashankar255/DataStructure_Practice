1490da81dc854e546b1681663009f83c8e8f2973
siba shankar sahu <sibashankar255@gmail.com>
2023-06-12 20:37:19 +0530

    recursion

--- src/recursion/Recursion.java
+++ src/recursion/Recursion.java
@@ -1,5 +1,7 @@
 package recursion;
 
+import java.util.HashSet;
+
 public class Recursion {
 
     public static void main(String[] args) {
@@ -7,11 +9,138 @@ public class Recursion {
         //sumOfNaturalNumbers(1,10,0);
         //System.out.println(factorial(5));
         //fibonacci(0,1,5);
-        System.out.println(calPower(2,5));
-        System.out.println(calPowerDynamic(2,5));
+        //System.out.println(calPower(2,5));
+        //System.out.println(calPowerDynamic(2,5));
+
+        //String str ="abcd efgh ijkl";
+        //printRev(str, str.length()-1);
+
+        //int arr[] = {1,3,3};
+        //System.out.println(isSorted(arr,0));
+
+        //String str = "axbcxxd";
+        //moveAllX(str,0,0,"");
+
+        //String str = "abbccda";
+        //removeDuplicates(str,0, "");
+
+        //String str = "abc";
+        //HashSet<String> set = new HashSet<>();
+        //subsequence(str,0,set);
 
     }
 
+    //print all permutations of a string
+    //"abc" -> abc, acb, bac, bca, cab, cba
+
+
+
+
+    //print all the unique subsequence of a string
+
+
+
+    //print all subsequences of a string
+    public static void subsequence(String str, int idx, String newString){
+        if (idx==str.length()){
+            System.out.println(newString);
+            return;
+        }
+
+        char currChar = str.charAt(idx);
+
+        subsequence(str, idx+1, newString+currChar);
+
+        subsequence(str, idx+1, newString);
+    }
+
+    //remove duplicates in a string
+    //"abbccda" -> "abcd"
+    public  static boolean[] map = new boolean[26];
+    public static void removeDuplicates(String str, int idx, String newString){
+        if (idx==str.length()){
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if (map[currChar-'a']){
+            removeDuplicates(str,idx+1, newString);
+        }else {
+            newString += currChar;
+            map[currChar -'a']=true;
+            removeDuplicates(str, idx+1, newString);
+        }
+    }
+
+
+
+    //move all 'x' to the end of the string
+    //"axbcxxd" -> "abcdxxx"
+    public static void moveAllX(String str, int idx, int count, String newString){
+        if (idx==str.length()){
+            for (int i=0; i<count; i++){
+                newString+='x';
+            }
+            System.out.println(newString);
+            return;
+        }
+        char currChar = str.charAt(idx);
+        if (currChar=='x'){
+            count++;
+            moveAllX(str,idx+1, count,newString);
+        }else {
+            newString += currChar; // newString = newString + currChar
+            moveAllX(str,idx+1,count,newString);
+        }
+    }
+
+
+    //check if an array is sorted (Strictly increasing)
+    public static boolean isSorted(int arr[], int idx){
+        if (idx==arr.length-1){
+            return true;
+        }
+        if (arr[idx] >= arr[idx+1]) {
+            //array is sorted till now
+            return isSorted(arr,idx+1);
+        }else {
+            return false;
+        }
+
+    }
+
+
+    //find the first and last occurance of the string
+    public static int first =-1;
+    public static int last =-1;
+    public static void findOccurance(String str, int idx, char element){
+
+        if (idx==str.length()){
+            System.out.println(first);
+            System.out.println(last);
+        }
+        char currChar = str.charAt(idx);
+        if (currChar==element){
+            if (first==-1){
+                first=idx;
+            }else {
+                last=idx;
+            }
+        }
+        findOccurance(str,idx+1, element);
+    }
+
+    //print the reverse of the string
+     public static void printRev(String str, int idx){
+        if (idx==0) {
+            System.out.print(str.charAt(idx));
+            return;
+        }
+        System.out.print(str.charAt(idx));
+        printRev(str,idx-1);
+     }
+
+     //
     public static int calPowerDynamic(int x, int n){
         if(n==0){
             return 1;
--- src/recursion/TowerOfHanoi.java
+++ src/recursion/TowerOfHanoi.java
@@ -1,5 +1,24 @@
 package recursion;
 
 public class TowerOfHanoi {
+    //Rules:-
+    //1. Only one disk transferred in 1 step
+    //2. Smaller disks are always kept on the top of larger disks
+    //O(2^n -1 ) ~ O(2^n)
 
+    public static void main(String[] args) {
+        int n=4;
+        towerOfHanoi(n, "Source","Helper","Destination");
+    }
+    public static void towerOfHanoi(int n, String src, String helper, String dest){
+
+        if (n==1){
+            System.out.println("transfer disk "+n+" from "+src+" to "+dest);
+            return;
+        }
+        towerOfHanoi(n-1, src,dest, helper);
+        System.out.println("transfer disk "+ n + " from "+ src+" to "+dest);
+        towerOfHanoi(n-1, helper, src, dest);
+
+    }
 }
